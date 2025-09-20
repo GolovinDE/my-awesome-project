5ef12f9d44b2358c7d1e02ca3e89402ff2786a38
Dmitriy <160262360+GolovinDE@users.noreply.github.com>
2025-09-20 13:06:12 +0300

    Проект бермуды
    Создал приложение консольное по расчёту площади прямоугольного треугольника

---
+++ PR_2GIT/PR_2GIT/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите длину первого катета:");
+        double cathetus1 = ReadPositiveDouble();
+
+        Console.WriteLine("Введите длину второго катета:");
+        double cathetus2 = ReadPositiveDouble();
+
+        double area = (cathetus1 * cathetus2) / 2;
+
+        Console.WriteLine($"Площадь прямоугольного треугольника: {area}");
+    }
+
+    static double ReadPositiveDouble()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double value) && value > 0)
+            {
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Введите положительное число.");
+            }
+        }
+    }
+}
